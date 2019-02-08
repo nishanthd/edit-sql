743e6e7bcfcaa17c15d48f604d718461983d768f
nishanthd <nishanth2837@gmail.com>
2019-02-08 11:46:45 +0530

    Renamed file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import SqlEditor from "./components/sqlEditor";
+import SqlEditor from "./components/sql-editor";
 
 
 class App extends Component {
